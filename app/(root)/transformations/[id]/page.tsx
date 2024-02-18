4b013ce0b89bf35a26ebc8eb7bd559f2946bf5a1
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-18 12:01:55 +0530

    feat: adding nextjs routing

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TransformationsPage = () => {
+  return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
+
+// http://localhost:3000/transformations/
+// http://localhost:3000/transformations/[id]
+// will hapepen through dynamic route, add new foulder [id]
