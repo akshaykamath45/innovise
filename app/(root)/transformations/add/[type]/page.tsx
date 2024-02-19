3c63282c81c9781e655e83edfa77b8b066a796ae
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-19 14:57:34 +0530

    style: including transformation types and header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationPage = () => {
-  return <div>AddTransformationPage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformationType = transformationTypes[type];
+  return (
+    <Header
+      title={transformationType.title}
+      subtitle={transformationType.subTitle}
+    />
+  );
 };
 
-export default AddTransformationPage;
+export default AddTransformationTypePage;
