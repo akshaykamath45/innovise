8e47f7011352898fcea128f5dedb01747ccc6448
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-19 16:09:10 +0530

    style: implementing transformation form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Header from "@/components/shared/Header";
 import { transformationTypes } from "@/constants";
-
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const transformationType = transformationTypes[type];
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const user = await getUserById(userId);
+
   return (
-    <Header
-      title={transformationType.title}
-      subtitle={transformationType.subTitle}
-    />
+    <>
+      <Header
+        title={transformationType.title}
+        subtitle={transformationType.subTitle}
+      />
+      <section className='mt-10'>
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformationType.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   );
 };
 
